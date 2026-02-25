6b0e310522aaee113d5aad9e6afa7591b209c3bb
Alejandro <aleeeejandroo13@gmail.com>
2026-02-25 12:59:25 +0100

    Reestructuracion carpetas

--- src/main/java/controllers/PedidoController.java
+++ src/main/java/controllers/PedidoController.java
@@ -1,4 +1,4 @@
-package main;
+package controllers;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
--- src/main/java/controllers/ReservaController.java
+++ src/main/java/controllers/ReservaController.java
@@ -1,4 +1,4 @@
-package main;
+package controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
